4a43fc4373b8c02b137711f551094d807a2f86a5
NachoM <NachoM@users.noreply.github.com>
2022-08-08 20:38:00 -0300

    desafio extra

--- clase4.js
+++ clase4.js
@@ -13,5 +13,13 @@ miPlata = miPlata - precioComida;
 console.log(`son las ${hora} , queres ir al piso ${piso} , y te van a quedar ${miPlata} pesos`);
 
 
+//Desafio Extra: invertir variables
 
 
+let cambioVariable;
+
+cambioVariable = hora;
+hora = piso;
+piso = cambioVariable;
+
+console.log(`ahora invertimos piso por hora, por lo que el piso es ${piso} y la hora es ${hora}`);
