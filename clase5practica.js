a77e93082af90f8c03deafbb17b04c1599404e24
NachoM <NachoM@users.noreply.github.com>
2022-08-10 20:37:16 -0300

    ejercicio practico con arrow functions

--- clase5practica.js
+++ clase5practica.js
@@ -3,19 +3,28 @@ const prompt = require("prompt-sync")({ sigint: true });
 
 //Micro desafío 1 --> function test1(x,y){return y - x} --> va a devolver y - x;
 
-function test1(x,y){
-    console.log(y - x);
-}
-
-test1(5,4);
-
 //Micro desafío 2 --> function test2(x,y){return x * 2 , console.log(x) , return x / 2} --> va a retornar únicamente el resultado de x * 2 pero no va a imprimir nada en pantalla
 
-function test2(x,y){
-    return x * 2;
-    console.log(x);
-    return x / 2;
-}
+// Crear una función que convierta pulgadas en centímetros.
+let pulgadas = prompt('ingrese las pulgadas: ');
+let pulgadasACentimetros = pulgadas => console.log(pulgadas * 2.54);
 
-test2(2,2);
+pulgadasACentimetros(pulgadas);
 
+//Crear una función que recibe un string y lo convierte en una URL.
+let stringUrl = prompt('ingrese el string: ');
+let convertorUrl = stringUrl => console.log(`https://www.${stringUrl}.com`);
+
+convertorUrl(stringUrl);
+
+//Crear una función que recibe un string y devuelve la misma frase pero con admiración.
+let stringAdmiracion = prompt('ingrese el string:');
+let convertorAdmiracion = stringAdmiracion => console.log(`¡${stringAdmiracion}!`);
+
+convertorAdmiracion(stringAdmiracion);
+
+//Crear una función que calcule la edad de los perros, considerando que 1 año para nosotros son 7 de ellos.
+let edad = prompt('ingrese la edad de su perro: ');
+let edadPerro = edad => console.log(edad * 7);
+
+edadPerro(edad);
