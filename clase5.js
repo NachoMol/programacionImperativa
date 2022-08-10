e51fee245b64bbbcb62d2eaa41830c077e976d18
NachoM <NachoM@users.noreply.github.com>
2022-08-10 20:02:40 -0300

    ejercicio 3 clase 5

--- clase5.js
+++ clase5.js
@@ -1,3 +1,5 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
 // Definir y crear una función es lo mismo, en este caso, definamos una que muestre en la consola un saludo. Luego, para poder ejecutarla, debemos invocarla o llamarla. Que sea algo así:
 
 function saludar() {
@@ -13,8 +15,15 @@ function saludar2(nombre){
 
 saludar2('ignacio');
 
+// Modificar la función para que solicite al usuario ingresar su nombre con prompt.
 
 
+let nombre = prompt('escribir nombre: ');
+function saludar3(nombre){
+console.log(`hola ${nombre}, tanto tiempo sin verte.`);
+}
+
+saludar3(nombre);
 
 
 //Micro desafío 1 --> function test1(x,y){return y - x} --> va a devolver y - x;
@@ -25,4 +34,13 @@ function test1(x,y){
 
 test1(5,4);
 
-//
+//Micro desafío 2 --> function test2(x,y){return x * 2 , console.log(x) , return x / 2} --> va a retornar únicamente el resultado de x * 2 pero no va a imprimir nada en pantalla
+
+function test2(x,y){
+    return x * 2;
+    console.log(x);
+    return x / 2;
+}
+
+test2(2,2);
+
