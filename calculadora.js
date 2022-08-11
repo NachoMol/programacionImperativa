830c9e97cf94cdf58f909a54cbffd95b335ec254
NachoMol <ignaciotmolina@gmail.com>
2022-08-11 20:55:43 -0300

    comentando testeos

--- calculadora.js
+++ calculadora.js
@@ -18,23 +18,23 @@ let generadorDePorcentaje = (numero1,numero2) => dividir(multiplicar(numero1,100
 
 console.log('--- Testeo de funciones ---')
 
-let numero1 = Number(prompt(`ingrese el primer valor: `));
-let numero2 = Number(prompt(`ingrese el segundo valor: `));
-let numero3 = Number(prompt(`ingrese el tercer valor: `));
-let total = Number(prompt(`ingrese el total: `))
-let porcentaje = Number(prompt(`ingrese el porcentaje: `))
+// let numero1 = Number(prompt(`ingrese el primer valor: `));
+// let numero2 = Number(prompt(`ingrese el segundo valor: `));
+// let numero3 = Number(prompt(`ingrese el tercer valor: `));
+// let total = Number(prompt(`ingrese el total: `))
+// let porcentaje = Number(prompt(`ingrese el porcentaje: `))
 
-console.log(sumar(numero1,numero2));
-console.log(restar(numero1,numero2));
-console.log(multiplicar(numero1,numero2));
-console.log(dividir(numero1,numero2));
+// console.log(sumar(numero1,numero2));
+// console.log(restar(numero1,numero2));
+// console.log(multiplicar(numero1,numero2));
+// console.log(dividir(numero1,numero2));
 
-console.log(cuadradoDeUnNumero(numero1));
+// console.log(cuadradoDeUnNumero(numero1));
 
-console.log(promedioDeTresNumeros(numero1,numero2,numero3));
+// console.log(promedioDeTresNumeros(numero1,numero2,numero3));
 
-console.log(calcularPorcentaje(total,porcentaje));
+// console.log(calcularPorcentaje(total,porcentaje));
 
-console.log(generadorDePorcentaje(numero1,numero2));
+// console.log(generadorDePorcentaje(numero1,numero2));
 
 
