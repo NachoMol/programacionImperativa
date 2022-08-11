17ec852c274212da8b3c1c20599b922cb44b4ef7
NachoM <NachoM@users.noreply.github.com>
2022-08-11 19:42:26 -0300

    nivel 2 calculadora

--- calculadora.js
+++ calculadora.js
@@ -1,10 +1,20 @@
 const prompt = require("prompt-sync")({ sigint: true });
 
-let sumar = (numero1,numero2) => numero1 + numero2;
+let sumar = (numero1,numero2) => console.log(numero1 + numero2);
 
-let restar = (numero1,numero2) => numero1 - numero2;
+let restar = (numero1,numero2) => console.log(numero1 - numero2);
 
-let multiplicar = (numero1,numero2) => numero1 * numero2;
+let multiplicar = (numero1,numero2) => console.log(numero1 * numero2);
 
-let dividir = (numero1,numero2) => numero1 / numero2;
+let dividir = (numero1,numero2) => console.log(numero1 / numero2);
+
+console.log('--- Testeo de funciones ---')
+
+let numero1 = prompt(`ingrese el primer valor: `);
+let numero2 = prompt(`ingrese el segundo valor: `);
+
+sumar(numero1,numero2);
+restar(numero1,numero2);
+multiplicar(numero1,numero2);
+dividir(numero1,numero2);
 
