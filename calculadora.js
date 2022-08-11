3e6f07ca9732b6d46293e924526ea3ba1ee038ea
NachoM <NachoM@users.noreply.github.com>
2022-08-11 19:14:02 -0300

    nivel 1 calculadora

---
+++ calculadora.js
@@ -0,0 +1,10 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
+let sumar = (numero1,numero2) => numero1 + numero2;
+
+let restar = (numero1,numero2) => numero1 - numero2;
+
+let multiplicar = (numero1,numero2) => numero1 * numero2;
+
+let dividir = (numero1,numero2) => numero1 / numero2;
+
