9dafa781f50b716615ff6b65a4d0f44921812808
NachoMol <ignaciotmolina@gmail.com>
2022-08-18 20:54:39 -0300

    subiendo practica clase7

---
+++ clase7Practica.js
@@ -0,0 +1,61 @@
+
+// Escribí un programa que le pida al usuario ingresar una frase y
+// la imprima en la consola.
+// Recordá que para pedirle al usuario que escriba una frase
+// debés utilizar el método prompt() y para escribir en la consola
+// debés utilizar el método console.log().
+
+// const prompt = require("prompt-sync")({ sigint: true });
+
+// let frase = prompt('ingrese una frase: ');
+
+// console.log(frase);
+
+//----------------------------------------------------------------------
+
+// Escribí un programa que le pregunte al usuario su nombre e imprima "Hola " seguido del nombre y un signo de exclamación.
+// Por ejemplo, si el usuario ingresa "Pedro" el programa debe imprimir en la consola "Hola Pedro!
+
+
+// let holaNombre = nombre => `Hola ${nombre}`;
+
+// console.log(holaNombre(`Nacho`));
+
+//----------------------------------------------------------------------
+
+// Escribí un programa que le pida al usuario ingresar un número, luego le pida un segundo número, e imprima en la consola la suma de los dos números que ingresó el usuario.
+
+// let suma = (numero1,numero2) => numero1 + numero2;
+
+// let resultado = suma(5,2);
+
+// console.log(resultado);
+
+//----------------------------------------------------------------------
+
+// Escribí un programa que le pida al usuario su año de nacimiento e imprima su edad actual en la consola con la frase "Tienes X años" (siendo X la cantidad de años). 
+
+// let edad = anioNacimiento => 2022 - anioNacimiento;
+
+// let resultado = edad(1999);
+
+// console.log(resultado);
+
+//----------------------------------------------------------------------
+
+// Escribí un programa que imprima los números pares del 0 al 100.
+
+// let numerosPares = () => {
+//     let i = 1;
+//     while (i <= 100 ){
+//         if(i % 2 == 0){
+//         console.log(i);
+//         }
+//         i++;
+//     }
+// }
+
+// numerosPares();
+
+//----------------------------------------------------------------------
+
