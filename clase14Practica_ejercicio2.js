02301544a66cecbc0e8a3159da8b42737e9717c1
NachoMol <ignaciotmolina@gmail.com>
2022-08-31 21:01:53 -0300

    subiendo consigna clase 14

---
+++ clase14Practica_ejercicio2.js
@@ -0,0 +1,51 @@
+// Tenemos como base un array de cuentas bancarias, donde a cada una la
+// representamos con un objeto literal. A partir de este array trabajaremos sobre
+// los siguientes enunciados, resolviendo de la forma que nos parezca más
+// adecuada
+
+const arrayCuentas =
+[
+{
+titular: "Arlene Barr",
+estaHabilitada: false,
+saldo: 3253.40,
+edadTitular: 33,
+tipoCuenta: "sueldo"
+},
+{
+titular: "Roslyn Torres",
+estaHabilitada: false,
+saldo: 3229.45,
+edadTitular: 27,
+tipoCuenta: "sueldo"
+},
+{
+titular: "Cleo Lopez",
+estaHabilitada: true,
+saldo: 1360.19,
+edadTitular: 34,
+tipoCuenta: "corriente"
+},
+{
+titular: "Daniel Malone",
+estaHabilitada: true,
+saldo: 3627.12,
+edadTitular: 30,
+tipoCuenta: "sueldo"
+},
+{
+    titular: "Ethel Leon",
+    estaHabilitada: true,
+    saldo: 1616.52,
+    edadTitular: 34,
+    tipoCuenta: "sueldo"
+    },
+    {
+    titular: "Harding Mitchell",
+    estaHabilitada: true,
+    saldo: 1408.68,
+    edadTitular: 25,
+    tipoCuenta: "corriente"
+    }
+    ]
+
